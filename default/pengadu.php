f9dbb48b349fcf6eac737b7541d4d7e6f00fef87
mhmmdputera <puteramhmmd@gmail.com>
2023-08-07 10:16:22 -0800

    first commit

---
+++ default/pengadu.php
@@ -0,0 +1,87 @@
+<?php
+	$sql = $koneksi->query("SELECT COUNT(id_pengaduan) as proses  from tb_pengaduan where status='Proses' and author='$data_id'");
+	while ($data= $sql->fetch_assoc()) {
+		$proses=$data['proses'];
+		}
+
+	$sql = $koneksi->query("SELECT COUNT(id_pengaduan) as tanggapi  from tb_pengaduan where status='Tanggapi' and author='$data_id'");
+  	while ($data= $sql->fetch_assoc()) {
+		$tangan=$data['tanggapi'];
+		}
+
+	$sql = $koneksi->query("SELECT COUNT(id_pengaduan) as selesai  from tb_pengaduan where status='Selesai' and author='$data_id'");
+  	while ($data= $sql->fetch_assoc()) {
+		$sel=$data['selesai'];
+		}
+
+?>
+<hr>
+<div>
+	<center>
+		<h1>Selamat Datang,
+			<?= $data_nama ?>-
+			<?= $data_level ?>
+		</h1>
+	</center>
+</div>
+<hr>
+<div class="col-md-4 col-sm-6 col-xs-6">
+	<div class="panel panel-back noti-box">
+		<span class="icon-box bg-color-red set-icon">
+			<i class="fa fa-bell"></i>
+		</span>
+		<div class="text-box">
+			<h1>
+				<?=  $proses; ?>
+			</h1>
+			<p>Pengaduan Menunggu</p>
+
+		</div>
+	</div>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-6">
+	<div class="panel panel-back noti-box">
+		<span class="icon-box bg-color-green set-icon">
+			<i class="fa fa-bell"></i>
+		</span>
+		<div class="text-box">
+			<h1>
+				<?=  $tangan; ?>
+			</h1>
+			<p>Pengaduan Ditanggapi</p>
+
+		</div>
+	</div>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-6">
+	<div class="panel panel-back noti-box">
+		<span class="icon-box bg-color-blue set-icon">
+			<i class="fa fa-bell"></i>
+		</span>
+		<div class="text-box">
+			<h1>
+				<?=  $sel; ?>
+			</h1>
+			<p>Pengaduan Selesai</p>
+
+		</div>
+	</div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<center>
+	<h4>
+		<a href="www.yandex.com" target="blank">Copyright @ 2023 - Kecamatan Banjarbaru Selatan</a>
+	</h4>
+</center>
